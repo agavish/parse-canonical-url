feced4a83405dd2818d094ff0273cd95ad8de772
Adam Gavish <agavish@gmail.com>
2015-07-28 16:47:16 +0300

    init

---
+++ test/parse-canonical-url_test.js
@@ -0,0 +1,58 @@
+/*global describe,it*/
+'use strict';
+
+var assert = require('assert')
+    , parseCanonicalUrl = require('../lib/parse-canonical-url.js');
+
+
+describe('Parse canonical url tests', function () {
+    it('parse canonical supported url if asked for', function (done) {
+
+        var askedUrl = "http://www.ynet.co.il";
+        return parseCanonicalUrl.canonical(askedUrl)
+            .then(function(result) {
+
+                assert.ok(result);
+                assert.notEqual(result, "");
+                assert.notEqual(askedUrl, result);
+                done();
+            });
+    });
+
+    it('parse alternate supported url if asked for', function (done) {
+
+        var askedUrl = "http://www.ynet.co.il";
+        return parseCanonicalUrl.alternate(askedUrl)
+            .then(function(result) {
+
+                assert.ok(result);
+                assert.notEqual(result, "");
+                assert.notEqual(askedUrl, result);
+                done();
+            });
+    });
+
+    it('fail to parse canonical bad url parameter', function (done) {
+
+        var askedUrl = "123";
+        return parseCanonicalUrl.canonical(askedUrl)
+            .then(function(result) {
+                assert.fail();
+            })
+            .catch(function(err) {
+                done();
+            });
+    });
+
+    it('fail to parse alternate bad url parameter', function (done) {
+
+        var askedUrl = "123";
+        return parseCanonicalUrl.alternate(askedUrl)
+            .then(function(result) {
+                assert.fail();
+            })
+            .catch(function(err) {
+                done();
+            });
+    });
+});
